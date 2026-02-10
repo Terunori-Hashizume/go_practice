4337e757c3a17aa47936c998576e9ef2d0db3967
Teru <26590954+Terunori-Hashizume@users.noreply.github.com>
2026-02-10 21:23:50 +0900

    10章2 v1.0.1

--- learning_go/prob-ch10/main.go
+++ learning_go/prob-ch10/main.go
@@ -1,5 +1,8 @@
 package main
 
+// Add 関数は、2つの整数 a と b を受け取り、それらの合計を返します。
+// より詳しい情報については、次のリンクを参照してください:
+// https://www.mathsisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
