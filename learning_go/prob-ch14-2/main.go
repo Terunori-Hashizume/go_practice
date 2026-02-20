db70ca9e17178275ad48b95ac190abc7e7697bbd
Teru <26590954+Terunori-Hashizume@users.noreply.github.com>
2026-02-20 19:18:26 +0900

    14章2修正

--- learning_go/prob-ch14-2/main.go
+++ learning_go/prob-ch14-2/main.go
@@ -14,21 +14,19 @@ func main() {
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	const format = "Sum: %d, Loops: %d, Reason: %s\n"
 
-	var loops int
 	var sum uint64
 
-	for {
-		i := r.Int63n(100_000_000)
-		sum += uint64(i)
-		loops++
+	for i := 1; ; i++ {
+		n := r.Int63n(100_000_000)
+		sum += uint64(n)
 
-		time.Sleep(2 * time.Millisecond) // 2秒経過させやすくするため
+		// time.Sleep(2 * time.Millisecond) // 2秒経過させやすくするため
 
 		if ctx.Err() != nil {
-			fmt.Printf(format, sum, loops, "2 seconds passed.")
+			fmt.Printf(format, sum, i, "2 seconds passed.")
 			return
-		} else if i == 1234 {
-			fmt.Printf(format, sum, loops, "1234 generated.")
+		} else if n == 1234 {
+			fmt.Printf(format, sum, i, "1234 generated.")
 			return
 		}
 	}
